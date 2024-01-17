e659ab01796b5ab61f386486657889db40bd8a26
mohamedEldeeb13 <mooel744@gmail.com>
2024-01-17 19:17:40 +0200

    Initial Commit

---
+++ e-commerce_UI/e_commerce_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_commerce_UIApp.swift
+//  e-commerce_UI
+//
+//  Created by Mohamed Abd Elhakam on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct e_commerce_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
